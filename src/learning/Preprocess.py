cd25199603b5990c9de6cd5e587489b4ce1acfcd
Woynert <61242172+Woynert@users.noreply.github.com>
2023-09-19 15:08:48 -0500

    feat: KNN multiclass classification (#17) (#18)
    * chore: Remove unused imports
    
    * feat: Group data by tags
    
    * feat: Balance data by dropping rows
    
    * feat: Get training and testing groups from tag data groups
    
    * feat: Calculate distance against other points
    
    * feat: Sort point distances
    
    * feat: Group and print contestants
    
    * fix: Use sys.path.append to import modules
    
    * feat: Point categorization
    
    * docs: Organize doc by sections

---
+++ src/learning/Preprocess.py
@@ -0,0 +1,74 @@
+import pandas as pd
+import numpy as np
+from typing import List, Dict, Tuple
+
+
+def separate_data_by_tags (
+        data: pd.DataFrame,
+        tags: List[str],
+        class_column: str
+    ) -> Dict[str, pd.DataFrame]:
+
+    data_groups: Dict[str, pd.DataFrame] = {}
+    group: pd.DataFrame
+
+    # iterate tags
+
+    for tag in tags:
+
+        group = pd.DataFrame()
+
+        for index, row in data.iterrows():
+
+            # add row to group if tag matches
+            if row[class_column] == tag:
+                group = pd.concat([group, pd.DataFrame(row).T])
+
+        data_groups[tag] = group
+        
+    return data_groups
+
+
+def balance_data_by_dropping_rows (
+        data: pd.DataFrame,
+        tags: List[str],
+        data_groups: Dict[str, pd.DataFrame]
+    ) -> pd.DataFrame:
+
+    # get min and max
+    
+    min = float("inf")
+    max = 0
+
+    for tag in tags:
+        size = len(data_groups[tag].index)
+        if size > max:
+            max = size
+        if size < min:
+            min = size
+
+    # drop
+
+    for tag in tags:
+        size_diff = len(data_groups[tag].index) - min
+        data_groups[tag] = data_groups[tag].iloc[size_diff:]
+
+
+def get_training_and_testing_groups (
+        percentage_train: float,
+        tags: List[str],
+        data_groups: Dict[str, pd.DataFrame]
+    ) -> Tuple[pd.DataFrame, pd.DataFrame]:
+
+    data_train = pd.DataFrame()
+    data_test  = pd.DataFrame()
+
+    data_train_size = len(data_groups[tags[0]].index) * percentage_train
+
+    for tag in tags:
+        sub_train, sub_test = np.split (data_groups[tag], [int(data_train_size)])
+
+        data_train = pd.concat([data_train, sub_train])
+        data_test = pd.concat([data_test, sub_test])
+
+    return data_train, data_test

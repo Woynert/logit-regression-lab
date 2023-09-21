d44b8dfbfa26ea0361a90776b2f9a89265649ac3
Carolina Beleño <Natalia.Beleno.2018@upb.edu.co>
2023-09-21 00:32:41 -0500

    Feat: Delete random amount

--- src/cleaning/OutlinerUtils.py
+++ src/cleaning/OutlinerUtils.py
@@ -1,6 +1,7 @@
 from typing import List
 import numpy as np
 import pandas as pd
+import random
 
 SENS = 1.5
 
@@ -8,27 +9,26 @@ def is_outliner (value, q1, q3, iqr) -> bool:
 
     return (value < (q1 - SENS * iqr)) or (value > (q3 + SENS * iqr))
 
-def get_outliners (data: List[float]) -> List[int]:
-    q1 = np.percentile(data, 25)
-    q3 = np.percentile(data, 75)
-    iqr = q3 - q1
+def get_outliner_indexes (data: pd.DataFrame) -> List[int]:
+    
+    outliner_indexes: List[int] = []
 
-    return [idx for idx, v in enumerate(data) if is_outliner(v, q1, q3, iqr)]
+    for column in data:
 
-def get_outliners_indexes (data: pd.DataFrame) -> List[int]:
+        vlist = data[str(column)].tolist()
+        q1 = np.percentile(vlist, 25)
+        q3 = np.percentile(vlist, 75)
+        iqr = q3 - q1
 
-    outliners: List[int] = []
+        for i in data[str(column)].index:
 
-    # get indexes for all outliners
+            v = data[str(column)][i]
+        
+            if is_outliner (v, q1, q3, iqr):
+                outliner_indexes.append(i)
 
-    for entry in data:
-        if str(entry) == "C33":
-            continue
-        new_outliners = get_outliners(data[str(entry)].tolist())
-        outliners.extend(new_outliners)
-
-    outliners = list(set(outliners))
-    return outliners
+    return list (set (outliner_indexes))   
+    
 
 def remove_outliners (data: pd.DataFrame) -> pd.DataFrame:
 
@@ -39,4 +39,24 @@ def remove_outliners (data: pd.DataFrame) -> pd.DataFrame:
     new_data = data.copy(True)
     new_data = new_data.drop(labels=outliners, axis=0)
     return new_data
+
+
+def delete_random_amount(data: pd.DataFrame
+    ) -> pd.DataFrame:
+    
+    outliners: List[int] = get_outliner_indexes(data)
+    
+    new_data = data.copy(True)
+    outliners_mixed = []
+    
+    d = int(20*len(data.index)/100)
+    
+    if len(outliners)>(40*len(data.index)/100):
+        outliners_mixed = random.sample(outliners,d)
+        
+        new_data = new_data.drop(labels=outliners_mixed, axis=0)
+    return new_data
+
+
+ 
 

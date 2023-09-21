d912b18181a0e57ced91a9a533fba188a41e2a9e
woynert <whayner.porras.2020@upb.edu.co>
2023-09-21 10:33:58 -0500

    feat: get ROC data

--- src/statistics/Statistics.py
+++ src/statistics/Statistics.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from typing import List
+from typing import List, Tuple, Dict
 import Categorization
 
 
@@ -34,3 +34,87 @@ def calculate_mape(
             mape += 1
 
     return (mape / len(data_test.index))
+
+
+def calculate_confusion_matrix(
+        k: int,
+        data_train: pd.DataFrame,
+        data_test: pd.DataFrame,
+        tags: List[str],
+        tag_column: str,
+        tag_objective: List[str]
+    ) -> Dict[str, int]:
+    
+    confusion_matrix = {
+        'TP': 0,
+        'FP': 0,
+        'TN': 0,
+        'FN': 0
+    }
+    
+    for i in range(len(data_test.index)):
+    
+        # get point
+        point = pd.DataFrame([data_test.iloc[i]])
+        #print(point)
+        
+        # predict using model
+        predicted_tag = Categorization.categorize_point(
+            data_train,
+            point,
+            k,  # k
+            tags,
+            tag_column
+        )
+        
+        # Etiqueta real
+        actual_tag = point.iloc[0][tag_column]
+        
+        #print(predicted_tag, ":", actual_tag)
+        # resultado de interes
+        if predicted_tag in tag_objective:
+        
+            # clasificacion correcta
+            if predicted_tag == actual_tag:
+                confusion_matrix['TP'] += 1
+            else:
+                confusion_matrix['FP'] += 1
+        
+        # no nos dio el valor de interes
+        else:
+            # clasificacion correcta
+            if predicted_tag == actual_tag:
+                confusion_matrix['TN'] += 1
+            else:
+                confusion_matrix['FN'] += 1
+    
+    return confusion_matrix 
+
+
+def get_roc_data_from_model (
+        k_list: List[int],
+        data_train: pd.DataFrame,
+        data_test: pd.DataFrame,
+        tags: List[str],
+        tag_column: str,
+        tag_objective: List[str]
+    ) -> Tuple[List[int], List[int]]:
+
+    listTPR = []
+    listFPR = []
+    
+    for k in k_list:
+
+        matrix = calculate_confusion_matrix(
+            k,
+            data_train,
+            data_test,
+            tags,
+            tag_column,
+            tag_objective
+        )
+
+        listTPR.append(matrix['TP'] / (matrix['TP'] + matrix['FN']))
+        listFPR.append(matrix['FP'] / (matrix['FP'] + matrix['TN']))
+
+    return listTPR, listFPR
